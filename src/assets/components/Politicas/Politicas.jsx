b92a5c806a6a2adc8fd36d7af4520fd21e3225ce
CESDE <kellyfuentes2007@gmail.com>
2024-03-15 18:31:31 -0500

    [ADD] ajustes

--- src/assets/components/Politicas/Politicas.jsx
+++ src/assets/components/Politicas/Politicas.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 function Politicas() {
   return (
     <section className='politicas'>
-        <p>Al hacer clic en «Continuar como» a continuación, aceptas las</p>
-        <p><a href="">Condiciones de uso</a>, la <a href="">Política de privacidad</a> y la <a href="">Política de cookies</a>de</p>
-        <p>LinkedIn.</p>
+      <div className='separador'></div>
+        <p>Al hacer clic en «Continuar como» a continuación, aceptas las <strong>Condiciones</strong></p>
+        <p><strong>de uso</strong>, la <strong>Política de privacidad</strong> y la <strong>Política de cookies</strong> de LinkedIn.</p>
+        
     </section>
   )
 }
