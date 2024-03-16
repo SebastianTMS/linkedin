ac58b3604a4262bd5766317a255216c2cf95b203
Sebastian Villada Jaramillo <sebastian.villada@tmsolutions.com.co>
2024-03-15 22:18:31 -0500

    [ADD] Maquetación Linkedin

--- src/assets/components/Politicas/Politicas.jsx
+++ src/assets/components/Politicas/Politicas.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 function Politicas() {
   return (
     <section className='politicas'>
-      <div className='separador'></div>
         <p>Al hacer clic en «Continuar como» a continuación, aceptas las <strong>Condiciones</strong></p>
         <p><strong>de uso</strong>, la <strong>Política de privacidad</strong> y la <strong>Política de cookies</strong> de LinkedIn.</p>
         
